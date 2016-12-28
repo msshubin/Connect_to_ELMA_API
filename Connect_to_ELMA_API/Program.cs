f7a2951c09364da283cdbbfd8f4420ce5a2dfff0
shubin <ms.shubin@ya.ru>
2016-12-28 11:43:18 +0400

    Add project files.

---
+++ Connect_to_ELMA_API/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Connect_to_ELMA_API.ELMA_SOAP;
+
+namespace Connect_to_ELMA_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Before you start, you need to remove service reference to my Web API 
+            // Add your own by click Add Service reference and enter string like "http://192.168.19.157/Modules/EleWise.ELMA.Workflow.Processes.Web/WFPWebService.asmx" 
+            // Where http://192.168.19.157 you must change to your own basic url of ELMA web-server
+            Console.Write("Enter login to ELMA: ");
+            string login = Console.ReadLine();
+            Console.Write("Enter password to ELMA: ");
+            string password = Console.ReadLine();
+
+            WFPWebServiceSoapClient client = new WFPWebServiceSoapClient();
+
+            // Simple example of getting information from active tasks from process Id 144910
+            foreach (var result in client.GetActiveTasks(login, password, 144910))
+            {
+                Console.WriteLine(result.CreationDate + " " + result.ExecutorName + " " + result.Subject);
+            }
+            Console.ReadKey();
+        }
+    }
+}
